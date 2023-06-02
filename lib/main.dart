ca02884e9144bc6fd3c69be1f5de3e49947b027c
Disza Jatnika <diszaid@gmail.com>
2023-06-02 13:37:51 +0700

    aplikasi android

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const latihan1());
+
+class latihan1 extends StatelessWidget {
+  const latihan1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Aplikasi "),
+          backgroundColor: Colors.amber,
+        ),
+        body: const Center(
+          child: Text(
+            "Halo Dunia",
+            style: TextStyle(
+              color: Colors.amber,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
